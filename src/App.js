24661518f412dce98c255567aa637191aede2715
acedomingo <75661215+acedomingo@users.noreply.github.com>
2023-07-27 16:29:02 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // packages
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, NavigationControl, Marker } from 'react-map-gl';
 import { useLazyQuery } from '@apollo/client';
 import { NEARBY_ROUTES } from './graphql/Queries';
-import GetRoutes from './components/GetRoutes';
+import Popup from 'reactjs-popup';
 // -----------------------------
 
 // Main Website
 
-function App() {
+const App = () => {
 
   // initial settings
   const [settings, setsettings] = useState({
@@ -16,27 +16,49 @@ function App() {
     doubleClickZoom: false
     });
   
-  // Nearby routes function
+  // ---------------- Nearby routes function -------------------------
+
   const [latitude, setLatitude] = useState("")
   const [longitude, setLongitude] = useState("")
-  
-  const [getNearbyRoutes, { loading, error, data }] = useLazyQuery(NEARBY_ROUTES);
-  console.log(data)
+  const [showPopup, setShowPopup] = useState(false);
+  const [noRoutes, setNoRoutes] = useState(false);
+  const [getNearbyRoutes, { error, data }] = useLazyQuery(NEARBY_ROUTES, {variables: {lat: latitude, lon: longitude}});
 
-  function handleClick(event) {
-    const coords = event.lngLat; // get coordinates
-    console.log('Clicked coordinates:', coords.lng, coords.lat);
+  if (error) return <p>Error: {error.message}</p>;
+
+  const allRoutes = data?.stopsByRadius?.edges?.flatMap((edge) => edge?.node?.stop?.routes) || []; // all of the routes
+
+  const uniqueRoutesSet = new Set();
+
+  // Filter out duplicates using the 'longName' property as the identifier
+  const routes = allRoutes.filter((route) => {
+    if (!uniqueRoutesSet.has(route.longName)) {
+      uniqueRoutesSet.add(route.longName);
+      return true;
+    }
+    return false;
+  });
+
+  console.log(routes);
+
+  async function handleClick(event) { // on double click
+    const coords = event.lngLat; // gets the coordinates of clicked location
     setLongitude(coords.lng);
     setLatitude(coords.lat);
-    getNearbyRoutes({variables: {lat: latitude, lon: longitude}})
+    await getNearbyRoutes() // requests query
+    console.log(coords);
 
-    return (
-      <Marker longitude={coords.lng} latitude={coords.lat}>
-      </Marker>
-    )
+    if (routes.length <= 0)
+      setNoRoutes(true);
+    else
+      setNoRoutes(false);
+
+    setShowPopup(true);
   }
-
+  
+  // -----------------------------------------------------------
   return (
+    <>
     <Map
       {...settings}
       style={{ width: window.innerWidth, height: window.innerHeight }}
@@ -50,7 +72,41 @@ function App() {
       onDblClick={handleClick}
     >
       <NavigationControl showCompass={false} />
+      <Marker latitude={latitude} longitude={longitude}></Marker>
     </Map>
+  
+    {/* Popup that displays the nearby routes */}
+    <Popup
+      open={showPopup}
+      onClose={() => setShowPopup(false)}
+      position="bottom left"
+      contentStyle={{
+        background: 'white',
+        width: '400px',
+        padding: '20px',
+        borderRadius: '4px',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+        textAlign: 'left',
+        position: 'fixed',
+        left: '20px',
+        bottom: '20px'
+      }}
+    >
+      <div>
+        <h1>Nearby Routes:</h1>
+        { noRoutes ? (
+            <p>There are no nearby routes.</p>
+            ) : (
+              <ul>
+              {routes.map((route) => (
+                <li key={route.gtfsId}>{route.longName}</li>
+              ))}
+              </ul> 
+            )
+        }
+      </div>
+    </Popup> 
+    </>
   );
 }
 

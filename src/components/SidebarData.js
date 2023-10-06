40155bf701c88bca0fab8bb9e8fb57bb2b4d66a8
acedomingo <75661215+acedomingo@users.noreply.github.com>
2023-10-06 12:06:49 +0800

    Update SidebarData.js

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import MenuIcon from '@mui/icons-material/Menu';
+import MapIcon from '@mui/icons-material/Map';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
 import EmojiTransportationIcon from '@mui/icons-material/EmojiTransportation';
 import HelpIcon from '@mui/icons-material/Help';
+import RouteIcon from '@mui/icons-material/Route';
 
 export const SidebarData = [
+    {
+        title: "Home",
+        icon: <MapIcon />,
+        link: "/home",
+    },
+    {
+        title: "Path Finding",
+        icon: <RouteIcon />,
+        link: "/pathfinding",
+    },
     {
         title: "Bus Routes",
         icon: <DirectionsBusIcon />,
@@ -17,13 +28,8 @@ export const SidebarData = [
         link: "/jeepneyroutes",
     },
     {
-        title: "Landmarks",
-        icon: <EmojiTransportationIcon />,
-        link: "/landmarks",
-    },
-    {
-        title: "Give Feedback",
+        title: "Feedback Form",
         icon: <HelpIcon />,
-        link: "/helpstack",
-    },
+        link: "https://forms.gle/hsQ11bMd9VJ2NNX96",
+    }
 ]

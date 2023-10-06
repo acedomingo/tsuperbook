a4c1cead81618f5daf59672377b7f9464107693d
MCMmndz13 <144411325+MCMmndz13@users.noreply.github.com>
2023-10-06 14:03:32 +0800

    Add files via upload

---
+++ JeepneyRoutes.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import '../App.css';
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
+const JeepneyRoutes = () => {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const handleSidebarToggle = () => { // toggles sidebar
+        setIsSidebarOpen(!isSidebarOpen);
+      };
+
+    return(
+       <>
+        <div className="Display">
+
+            <div className="App">
+                <Navbar onSidebarToggle={handleSidebarToggle} />
+                {isSidebarOpen && <Sidebar onClose={handleSidebarToggle}/>}
+            </div>
+
+</div>
+       </>
+    )
+}
+
+export default JeepneyRoutes

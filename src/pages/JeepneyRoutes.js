2233e1f13c78763fa025d2e6d2c8cb460bcf60a1
MCMmndz13 <144411325+MCMmndz13@users.noreply.github.com>
2023-10-13 13:14:20 +0800

    Update JeepneyRoutes.js

--- src/pages/JeepneyRoutes.js
+++ src/pages/JeepneyRoutes.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import '../App.css';
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import Tile from "../components/Tile";
+import TileGrid from "../components/TileGrid";
 
 const JeepneyRoutes = () => {
 
@@ -11,17 +13,28 @@ const JeepneyRoutes = () => {
         setIsSidebarOpen(!isSidebarOpen);
       };
 
+      const tiles = [
+        { title: "Tile 1", content: "This is the content for Tile 1." },
+        { title: "Tile 2", content: "This is the content for Tile 2." },
+        { title: "Tile 3", content: "This is the content for Tile 3." },
+        { title: "Tile 4", content: "This is the content for Tile 4." },
+        // Add more tiles as needed
+    ];
+
     return(
-       <>
+
         <div className="Display">
 
-            <div className="App">
-                <Navbar onSidebarToggle={handleSidebarToggle} />
+            <Navbar onSidebarToggle={handleSidebarToggle} />
+            <div className={`Content ${isSidebarOpen ? 'shifted' : ''}`}>
+                
                 {isSidebarOpen && <Sidebar onClose={handleSidebarToggle}/>}
+
+                <TileGrid tiles={tiles} />
             </div>
 
-</div>
-       </>
+        </div>
+
     )
 }
 

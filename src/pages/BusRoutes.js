7d5aa5a198b2ab152fb245835fc433d59bf2f28e
MCMmndz13 <144411325+MCMmndz13@users.noreply.github.com>
2023-10-24 15:10:42 +0800

    automatic tiles function for BusRoutes.js
    - creates tiles automatically for every route data with type: "Bus"
    - display name for each route as Title in tile

--- src/pages/BusRoutes.js
+++ src/pages/BusRoutes.js
@@ -4,41 +4,32 @@ import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import Tile from "../components/Tile";
 import TileGrid from "../components/TileGrid";
+import RouteData from "../components/RouteData";
 
 const BusRoutes = () => {
-
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const handleSidebarToggle = () => { // toggles sidebar
         setIsSidebarOpen(!isSidebarOpen);
-      };
+    };
 
-      const tiles = [
-        { title: "Tile 1", content: "This is the content for Tile 1." },
-        { title: "Tile 2", content: "This is the content for Tile 2." },
-        { title: "Tile 3", content: "This is the content for Tile 3." },
-        { title: "Tile 4", content: "This is the content for Tile 4." },
-        { title: "Tile 5", content: "This is the content for Tile 5." },
-        { title: "Tile 6", content: "This is the content for Tile 6." },
-        
-        // Add more tiles as needed
-    ];
+    // Filter the routes with type "Bus" and create tiles for each of them
+    const busRoutes = RouteData.filter(route => route.type === "Bus");
+    const tiles = busRoutes.map(route => ({
+        title: route.name,
+    }));
 
-    return(
-
+    return (
         <div className="Display">
 
             <Navbar onSidebarToggle={handleSidebarToggle} />
             <div className={`Content ${isSidebarOpen ? 'shifted' : ''}`}>
-                
-                {isSidebarOpen && <Sidebar onClose={handleSidebarToggle}/>}
-
+                {isSidebarOpen && <Sidebar onClose={handleSidebarToggle} />}
                 <TileGrid tiles={tiles} />
             </div>
 
         </div>
-
-    )
+    );
 }
 
-export default BusRoutes
+export default BusRoutes;

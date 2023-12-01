37102c7152f78efc36e79e56db3a57cf71967b1f
MCMmndz13 <144411325+MCMmndz13@users.noreply.github.com>
2023-12-01 12:19:12 +0800

    component to open feedback form in new tab added to sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,42 @@
-import React from 'react'
+import React from 'react';
 import "../App.css";
-import {SidebarData} from './SidebarData';
+import { SidebarData } from './SidebarData';
 
 const Sidebar = ({ onClose }) => {
     const handleClose = () => {
-      onClose();
-    }
+        onClose();
+    };
+
     return (
         <div className="Sidebar">
             <div style={{ height: 46 }} />
             <ul className="SidebarList">
-            {SidebarData.map((val,key)=> {
-                return (
-                <li key={key} 
-                    className="Row"
-                    id={window.location.pathname == val.link ? "active" : ""}
-                    onClick={()=> {
-                        window.location.pathname = val.link
-                    }}
-                > 
-                    <div id="icon">
-                        {val.icon}
-                    </div>
-                    <div id="title">
-                        {val.title}
-                    </div>
-                </li>
-                )
-            })}
+                {SidebarData.map((val, key) => {
+                    return (
+                        <li key={key}
+                            className="Row"
+                            id={window.location.pathname === val.link ? "active" : ""}
+                            onClick={() => {
+                                if (val.externalLink == true) {
+                                    // Open external link in a new window
+                                    window.open(val.link, '_blank');
+                                } else {
+                                    window.location.pathname = val.link;
+                                }
+                            }}
+                        >
+                            <div id="icon">
+                                {val.icon}
+                            </div>
+                            <div id="title">
+                                {val.title}
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
-        
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;

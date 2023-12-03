0092d2cfc02693349a18b6d9b7bbd31e15b7d521
acedomingo <75661215+acedomingo@users.noreply.github.com>
2023-12-03 21:06:21 +0800

    Update Tile.js

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,13 +1,20 @@
-// Tile.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Tile = ({ title, content }) => {
-    return (
-        <div className="Tile">
-            <h2>{title}</h2>
-            <p>{content}</p>
-        </div>
-    );
-}
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/?selectRoute=${title}`);
+    console.log("Clicked:", title);
+  };
+
+  return (
+    <div className="Tile" onClick={handleClick}>
+        <h2>{title}</h2>
+        <p>{content}</p>
+    </div>
+  );
+};
 
 export default Tile;

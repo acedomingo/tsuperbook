40c4c6a368dcb2c3f095688982ce7973992d5981
acedomingo <75661215+acedomingo@users.noreply.github.com>
2024-01-02 21:44:03 +0800

    Update RouteInfo.js

--- src/components/RouteInfo.js
+++ src/components/RouteInfo.js
@@ -3,53 +3,53 @@ import '../App.css';
 import { ROUTE_DETAILS } from '../graphql/Queries';
 import { useQuery } from '@apollo/client';
 
-const RouteInfo = ({ selectRoute, onClosePopup, differentRoute, enterHighlight, exitHighlight }) => {
-  console.log('Route Info:', selectRoute);
-
-  const handleClick = (val) => {
-    console.log("Route:", val)
-  }
-
+const RouteInfo = ({ selectRoute, onClosePopup, differentRoute, enterHighlight, exitHighlight, selectStop, hoverStop, leaveStop }) => {
   // Use optional chaining to prevent errors when selectRoute is null
   const { loading, error, data } = useQuery(ROUTE_DETAILS, {
     variables: { route_id: selectRoute?.id ? `1:LTFRB_${selectRoute.id}` : null },
   });
 
-  const [stops, setStops] = useState([]);
+  const [forwardStops, setForwardStops] = useState([]);
+  const [returnStops, setReturnStops] = useState([]);
   const [landmarks, setLandmarks] = useState(selectRoute?.landmarks || []);
   const [nearbyRoutes, setNearbyRoutes] = useState(null);
 
   useEffect(() => {
     if (data) {
-      console.log('Query:', data);
-  
-      // Use optional chaining to safely access properties
-      const stopsData = data.route?.stops || [];
-      setStops(stopsData);
-  
-      // Extract nearby routes from each stop and remove duplicates
-      console.log("Overlapping routes:", nearbyRoutes);
-      const uniqueRoutesSet = new Set();
-      const allNearbyRoutes = stopsData.flatMap((stop) =>
-        stop.routes?.map((route) => {
-          const routeObject = { name: route.longName, id: route.gtfsId };
-          if (!uniqueRoutesSet.has(routeObject.id)) {
-            uniqueRoutesSet.add(routeObject.id);
-            return routeObject;
-          }
-          return null;
-        }) || []
-      );
-  
-      // Filter out routes with the same name as selectRoute.name
-      const filteredNearbyRoutes = allNearbyRoutes.filter(
-        (route) => route && route.name !== selectRoute.name
-      );
-  
-      setNearbyRoutes(filteredNearbyRoutes);
-      console.log('Nearby Routes:', filteredNearbyRoutes);
+      const forwardStopsData = data.route?.trips
+        ?.find((trip) => trip.directionId == "1")
+        ?.stops || [];
+      setForwardStops(forwardStopsData);
+
+      const returnStopsData = data.route?.trips
+        ?.find((trip) => trip.directionId == "0")
+        ?.stops || [];
+      setReturnStops(returnStopsData);
+
+      const allNearbyRoutes = extractNearbyRoutes(forwardStopsData.concat(returnStopsData));
+      setNearbyRoutes(allNearbyRoutes);
+      console.log("Nearby Routes:", allNearbyRoutes);
     }
   }, [data, selectRoute]);
+  
+  const extractNearbyRoutes = (stopsData) => {
+    const uniqueRoutesSet = new Set();
+    const allNearbyRoutes = stopsData.flatMap((stop) =>
+    stop.routes?.map((route) => {
+      const routeObject = { name: route.longName, id: route.gtfsId };
+      if (!uniqueRoutesSet.has(routeObject.id)) {
+        uniqueRoutesSet.add(routeObject.id);
+        return routeObject;
+      }
+      return null;
+      }) || []
+    );
+
+    return allNearbyRoutes.filter(
+      (route) => route && route.name !== selectRoute.name
+    );
+  };
+  
   // If selectRoute is null, return null or an empty fragment
   if (!selectRoute) {
     return null;
@@ -62,13 +62,35 @@ const RouteInfo = ({ selectRoute, onClosePopup, differentRoute, enterHighlight, 
       </button>
       <h1>{selectRoute.name}</h1>
       <h2>{`(${selectRoute.type})`}</h2>
-      <h3>Stops</h3>
+      <h3>Forward Trip Stops</h3>
       <ul>
-        {stops.map((stop) => (
-          <li key={stop.name}>{stop.name}</li>
+        {forwardStops.map((stop) => (
+          <li
+            key={stop.name}
+            onClick={() => selectStop(stop)}
+            onMouseEnter={() => hoverStop(stop)}
+            onMouseLeave={leaveStop}
+            className="routeName"
+          >
+            {stop.name}
+          </li>
         ))}
       </ul>
-      <h3>Landmarks</h3>
+      <h3>Return Trip Stops</h3>
+      <ul>
+        {returnStops.map((stop) => (
+          <li
+            key={stop.name}
+            onClick={() => selectStop(stop)}
+            onMouseEnter={() => hoverStop(stop)}
+            onMouseLeave={leaveStop}
+            className="routeName"
+          >
+            {stop.name}
+          </li>
+        ))}
+      </ul>
+      <h3>Visitable Landmarks</h3>
       <ul>
         {landmarks.length > 0 ? (
           landmarks.map((landmark) => <li key={landmark.id}>{landmark.name}</li>)
